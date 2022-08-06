eae154273dbb9c83147020363341ca330b205244
minjeong Kim <meanjeong92@gmail.com>
2022-08-06 16:24:54 +0900

    kmj 08/06 16:24

---
+++ src/main/java/com/oracle/S202207/handler/kmj/SocketHandler.java
@@ -0,0 +1,10 @@
+package com.oracle.S202207.handler.kmj;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+	
+}
